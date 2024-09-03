2495ec0a8618a05b3be3eb5cff0dc7af7b7b547e
anish-singh-rawat <www.computernetwork@gmail.com>
2024-09-03 12:55:23 +0530

    add project page carts

--- src/icplaunchpad_frontend/src/AllRoutes.jsx
+++ src/icplaunchpad_frontend/src/AllRoutes.jsx
@@ -4,12 +4,14 @@ import Home from "./Pages/Home";
 import LaunchCoin from "./Pages/LaunchCoin"
 import PrivateLayout from './Layout/PrivateLayout';
 import Projects from './Pages/Projects/Projects';
+import ProjectList from './Pages/Project/ProjectList';
 
 function AllRoutes() {
   return (
     <Routes>
       <Route path="/" element={<PrivateLayout>  <Home /></PrivateLayout>} />
       <Route path="/projects" element={<PrivateLayout> <Projects /> </PrivateLayout>} />
+      <Route path="/project" element={<PrivateLayout> <ProjectList /> </PrivateLayout>} />
       <Route path="/launchCoin" element={<PrivateLayout> <LaunchCoin /> </PrivateLayout>} />
     </Routes>
   )
