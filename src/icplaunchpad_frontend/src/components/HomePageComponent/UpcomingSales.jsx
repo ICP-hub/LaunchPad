eca4d5e627e386a6485c0e2d1137f5546abfe142
chandan-quadb <chandan.tech@quadbtech.com>
2024-11-26 14:03:55 +0530

    layout fix upcoming

--- src/icplaunchpad_frontend/src/components/HomePageComponent/UpcomingSales.jsx
+++ src/icplaunchpad_frontend/src/components/HomePageComponent/UpcomingSales.jsx
@@ -7,12 +7,12 @@ const UpcomingSales = React.forwardRef((props, ref) => {
   const navigate = useNavigate();
   const actor = useSelector((currState) => currState.actors.actor);
 
- const salesData= useSelector((state)=>state.upcomingSales.data)
+  const salesData = useSelector((state) => state.upcomingSales.data)
 
   // Handle navigation to the projects page
   const handleViewMoreClick = () => {
     if (salesData.length > 0) {
-      navigate("/projects", { state: { salesData:salesData[0], sale_Type: "Upcoming" } });
+      navigate("/projects", { state: { salesData: salesData[0], sale_Type: "Upcoming" } });
     }
   };
 
@@ -21,7 +21,7 @@ const UpcomingSales = React.forwardRef((props, ref) => {
       ref={ref}
       className="upcoming-sales h-full mt-8 md:mb-[5%] lg:mb-0 sm4:mb-3 py-[5%]"
     >
-      <div className="w-7xl flex justify-between items-center px-[8%] mb-10 mx-auto">
+      <div className="flex justify-between items-center px-[4%] mb-10">
         <h2 className="text-white font-bold font-posterama text-[24px] xxs1:text-3xl">
           UPCOMING SALES
         </h2>
@@ -35,29 +35,33 @@ const UpcomingSales = React.forwardRef((props, ref) => {
         )}
       </div>
 
-      <div className="flex lg:flex-row flex-col flex-wrap items-center w-7xl md:px-[8%] mx-auto  gap-24 justify-start">
-        {salesData.length > 0 ? (
-          salesData.slice(0, 3).map((sale, index) => (
-            <ProjectCard
-              key={index}
-              initial_Total_supply={ sale[1] || null}
-              projectData={sale[0]}
-              saleType="upcoming"
-              index={index}
-            />
-          ))
-        ) : (
-          <h1 className="text-xl mx-auto my-16">Data Not Found...</h1>
-        )}
-        {salesData.length > 3 && (
-          <button
-            onClick={handleViewMoreClick}
-            className="text-white mt-4 xxs1:hidden font-posterama underline text-[20px] xxs1:text-xl"
-          >
-            LOAD MORE
-          </button>
-        )}
-      </div>
+        {/* <div className="flex lg:flex-row flex-col flex-wrap items-center  m-auto gap-12 justify-start"> */}
+      <div
+        className={`flex lg:flex-row flex-col flex-wrap items-center m-auto gap-12 ${salesData.length >= 3 ? 'justify-center' : 'justify-start'}`}
+      >
+          {salesData.length > 0 ? (
+            salesData.slice(0, 3).map((sale, index) => (
+              <ProjectCard
+                key={index}
+                initial_Total_supply={sale[1] || null}
+                projectData={sale[0]}
+                saleType="upcoming"
+                index={index}
+              />
+            ))
+          ) : (
+            <h1 className="text-xl mx-auto my-16">Data Not Found...</h1>
+          )}
+          {salesData.length > 3 && (
+            <button
+              onClick={handleViewMoreClick}
+              className="text-white mt-4 xxs1:hidden font-posterama underline text-[20px] xxs1:text-xl"
+            >
+              LOAD MORE
+            </button>
+          )}
+        </div>
+     
     </div>
   );
 });
